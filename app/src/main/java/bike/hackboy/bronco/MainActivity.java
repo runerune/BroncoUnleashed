71a46e512d0a5228829ba854dde14ace8932c1f5
runerune <guildbot@bezkr.es>
2021-03-16 22:41:06 +0100

    Block back button navigation between fragments

--- app/src/main/java/bike/hackboy/bronco/MainActivity.java
+++ app/src/main/java/bike/hackboy/bronco/MainActivity.java
@@ -124,6 +124,12 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // --------------------------------------------------
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
 
 }

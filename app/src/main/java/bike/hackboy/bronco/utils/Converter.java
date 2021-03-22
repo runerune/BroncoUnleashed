bfc7b946e4898c236f475760365f8feac5ecd991
runerune <guildbot@bezkr.es>
2021-03-22 02:15:33 +0100

    add rot13 converter

--- app/src/main/java/bike/hackboy/bronco/utils/Converter.java
+++ app/src/main/java/bike/hackboy/bronco/utils/Converter.java
@@ -23,4 +23,20 @@ public class Converter {
 
 		return new int[]{hours, minutes, secs};
 	}
+
+	public static String rot13(String input) {
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < input.length(); i++) {
+			char c = input.charAt(i);
+
+			if       (c >= 'a' && c <= 'm') c += 13;
+			else if  (c >= 'A' && c <= 'M') c += 13;
+			else if  (c >= 'n' && c <= 'z') c -= 13;
+			else if  (c >= 'N' && c <= 'Z') c -= 13;
+
+			sb.append(c);
+		}
+		return sb.toString();
+	}
 }

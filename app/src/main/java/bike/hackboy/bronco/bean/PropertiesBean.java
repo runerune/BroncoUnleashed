896a778d15493e85d6317fd6fa7a83d7d07368ca
runerune <guildbot@bezkr.es>
2021-03-23 23:03:40 +0100

    add "last" boolean field

--- app/src/main/java/bike/hackboy/bronco/bean/PropertiesBean.java
+++ app/src/main/java/bike/hackboy/bronco/bean/PropertiesBean.java
@@ -3,6 +3,7 @@ package bike.hackboy.bronco.bean;
 public class PropertiesBean {
 	private String name;
 	private String value;
+	private boolean last = false;
 
 	public String getName() {
 		return name;
@@ -20,11 +21,20 @@ public class PropertiesBean {
 		this.value = value;
 	}
 
+	public boolean isLast() {
+		return last;
+	}
+
+	public void setLast(boolean last) {
+		this.last = last;
+	}
+
 	@Override
 	public String toString() {
 		return "PropertiesBean{" +
 			"name='" + name + '\'' +
 			", value='" + value + '\'' +
+			", last=" + last +
 			'}';
 	}
 }

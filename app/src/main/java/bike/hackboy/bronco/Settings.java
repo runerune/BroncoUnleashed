de0278cc4bf689776307942d72495c87fe44b900
runerune <guildbot@bezkr.es>
2021-03-27 15:45:00 +0100

    Add settings activity to group all the settings in a single place

---
+++ app/src/main/java/bike/hackboy/bronco/Settings.java
@@ -0,0 +1,14 @@
+package bike.hackboy.bronco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+	}
+}

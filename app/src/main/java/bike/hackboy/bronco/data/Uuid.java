e33a156d0485fe5d75092c5cc2c8164dfc707d1b
runerune <guildbot@bezkr.es>
2021-11-10 01:12:50 +0100

    Add uuids for dfc retrieval

--- app/src/main/java/bike/hackboy/bronco/data/Uuid.java
+++ app/src/main/java/bike/hackboy/bronco/data/Uuid.java
@@ -7,6 +7,7 @@ public class Uuid {
 	public static final String characteristicDashboardString = "C0B0A00A-18EB-499D-B266-2F2910744274";
 	public static final String characteristicSettingsReadString = "6E400003-B5A3-F393-E0A9-E50E24DCCA9E";
 	public static final String characteristicSettingsWriteString = "6E400002-B5A3-F393-E0A9-E50E24DCCA9E";
+	public static final String characteristicDfcRequestString = "C0B0A005-18EB-499D-B266-2F2910744274";
 
 	public static final UUID notificationDescriptor = UUID.fromString("00002902-0000-1000-8000-00805F9B34FB");
 
@@ -17,5 +18,7 @@ public class Uuid {
 	public static final UUID characteristicSettingsRead = UUID.fromString(Uuid.characteristicSettingsReadString);
 	public static final UUID characteristicUnlock = UUID.fromString(Uuid.characteristicUnlockString);
 	public static final UUID characteristicDashboard = UUID.fromString(Uuid.characteristicDashboardString);
+
+	public static final UUID characteristicDfcRequest = UUID.fromString(Uuid.characteristicDfcRequestString);
 
 }
